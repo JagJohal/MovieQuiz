6b4a84e8ca7bf38f7838ff513649791028d92dc5
j.johal@wlc.ac.uk <j.johal@wlc.ac.uk>
2023-04-17 13:35:41 +0100

    Corrected errors - its now running

--- MovieQuizTemplate/frmMain.vb
+++ MovieQuizTemplate/frmMain.vb
@@ -1,6 +1,6 @@
 ﻿Public Class frmMain
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
-        `
+
     End Sub
 
     Private Sub PictureBox5_Click(sender As Object, e As EventArgs) Handles PictureBox5.Click
