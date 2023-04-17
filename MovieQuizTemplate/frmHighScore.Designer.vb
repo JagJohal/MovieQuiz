04c105378fb48bc405b6fadd6f957205359ca77c
j.johal@wlc.ac.uk <j.johal@wlc.ac.uk>
2023-04-17 13:31:30 +0100

    Updated code

--- MovieQuizTemplate/frmHighScore.Designer.vb
+++ MovieQuizTemplate/frmHighScore.Designer.vb
@@ -26,6 +26,8 @@ Partial Class frmHighScore
         Me.lstHighScore = New System.Windows.Forms.ListBox()
         Me.btnAgain = New System.Windows.Forms.Button()
         Me.btnQuit = New System.Windows.Forms.Button()
+        Me.lblmessage = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
@@ -66,11 +68,32 @@ Partial Class frmHighScore
         Me.btnQuit.Text = "QUIT"
         Me.btnQuit.UseVisualStyleBackColor = True
         '
+        'lblmessage
+        '
+        Me.lblmessage.AutoSize = True
+        Me.lblmessage.Location = New System.Drawing.Point(198, 309)
+        Me.lblmessage.Name = "lblmessage"
+        Me.lblmessage.Size = New System.Drawing.Size(0, 13)
+        Me.lblmessage.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Label2.ForeColor = System.Drawing.SystemColors.AppWorkspace
+        Me.Label2.Location = New System.Drawing.Point(212, 358)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(59, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "lblmessage"
+        '
         'frmHighScore
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblmessage)
         Me.Controls.Add(Me.btnQuit)
         Me.Controls.Add(Me.btnAgain)
         Me.Controls.Add(Me.lstHighScore)
@@ -86,4 +109,6 @@ Partial Class frmHighScore
     Friend WithEvents lstHighScore As ListBox
     Friend WithEvents btnAgain As Button
     Friend WithEvents btnQuit As Button
+    Friend WithEvents lblmessage As Label
+    Friend WithEvents Label2 As Label
 End Class
--- MovieQuizTemplate/frmHighScore.vb
+++ MovieQuizTemplate/frmHighScore.vb
@@ -7,4 +7,17 @@
         frmMain.Show()
         Me.Hide()
     End Sub
+
+    Public Sub doscore()
+        If (playerScore < 1) Then
+            lblmessage.text = "sorry Your Score Was Not Good Enough"
+        Else
+            lblmessage.text = "Congratulations Your Score Will Be Added To The Hall of Fame "
+            lstHighScore.Items.Add(playerName & vbTab & vbTab & playerScore)
+        End If
+    End Sub
+
+    Private Sub frmHighScore_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
