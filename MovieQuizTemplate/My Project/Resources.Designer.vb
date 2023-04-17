67d66aab9345022e7f633b87f8b773d402c9dca2
j.johal@wlc.ac.uk <j.johal@wlc.ac.uk>
2023-04-17 11:35:31 +0100

    Question 1 - Audrey Hepburn

--- MovieQuizTemplate/My Project/Resources.Designer.vb
+++ MovieQuizTemplate/My Project/Resources.Designer.vb
@@ -63,6 +63,26 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property hep1() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("hep1", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property hep2() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("hep2", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property pic1() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("pic1", resourceCulture)
